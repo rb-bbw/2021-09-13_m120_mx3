628870203028c855a3114cd23b22ac9c01db917d
rb-bbw <remo.bergamin@bbwch.onmicrosoft.com>
2021-10-25 08:50:19 +0200

    Redirect to /search

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container} from "react-bootstrap";
 import SearchBands from "./components/SearchBands";
 import ShowProfile from "./components/ShowProfile";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Redirect, Route} from "react-router-dom";
 
 function App() {
     return (
         <Container>
             <Router>
-                <Route exact path="/" redirect/>
+                <Route exact path="/" render={() => {
+                    return ( <Redirect to="/search" /> )
+                }}/>
                 <Route exact path="/search" component={SearchBands}/>
                 <Route exact path="/profile/:name" component={ShowProfile}/>
             </Router>
